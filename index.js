72f1b01d87e87e47d990a8aff25a8eefc5c12f9c
Karl Brightman <karl.brightman@envato.com>
2015-11-30 21:15:37 +0800

    Added heroku setup

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ let app = express();
 app.set('port', process.env.PORT || 5000);
 app.get('/', function(req, res) { res.send('hello world'); })
 app.listen(app.get('port'), function () {
-  console.log('App listening on port', app.get('port'))
+  console.log('Application is running on port ' + app.get('port'));
 })
