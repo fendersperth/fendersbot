8b1ab36c4998fb0d9c3232e47d44d0d891e976bb
Karl Brightman <karl.brightman@envato.com>
2016-01-09 12:40:27 +0800

    Use camelcase for taylorSwift

--- test/responses/taylor_swift_test.js
+++ test/responses/taylor_swift_test.js
@@ -2,13 +2,13 @@ const assert = require('assert');
 const sinon = require('sinon');
 const fakeBot = require('../support/fake_bot');
 const stubMessage = require('../support/stub_message');
-const taylorswift = require('../../lib/slack/responses/taylor_swift');
+const taylorSwift = require('../../lib/slack/responses/taylor_swift');
 
-describe('taylorswift', function() {
+describe('taylorSwift', function() {
   it('adds a reaction', function() {
     const spy = sinon.spy(fakeBot.api.reactions, 'add');
     const message = stubMessage('Who likes Taylor Swift?');
-    taylorswift.callback(fakeBot, message);
+    taylorSwift.callback(fakeBot, message);
     sinon.assert.calledOnce(spy);
     sinon.assert.calledWith(spy, {
       name: 'taylorswift',
